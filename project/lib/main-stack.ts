f5358cbf6f6c775d62549a11ee8e61ffe0df81a6
khara <d.kurihara0318@gmail.com>
2022-03-30 16:09:37 +0900

    add vpc&bastion

---
+++ project/lib/main-stack.ts
@@ -0,0 +1,19 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { VpcStack } from './vpc-stack';
+import { BastionStack } from './bastion-stack';
+
+export class MainStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+    const envType = scope.node.tryGetContext("envType");
+
+    const vpcStack = new VpcStack(scope, 'VpcStack', {
+        stackName: `vpc-stack-${envType}`
+      });
+
+    new BastionStack(scope, 'BastionStack', vpcStack.vpc, {
+        stackName: `bastion-stack-${envType}`
+    })
+  }
+}
