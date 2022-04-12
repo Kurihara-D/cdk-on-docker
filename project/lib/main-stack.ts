fc0d3487223a7ec8c66fdb87cfdca09199893719
khara <d.kurihara0318@gmail.com>
2022-04-12 16:06:38 +0900

    add Rds stack

--- project/lib/main-stack.ts
+++ project/lib/main-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { VpcStack } from './vpc-stack';
 import { BastionStack } from './bastion-stack';
+import { RdsStack } from './rds-stack';
 
 export class MainStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -14,6 +15,10 @@ export class MainStack extends Stack {
 
     new BastionStack(scope, 'BastionStack', vpcStack.vpc, {
         stackName: `bastion-stack-${envType}`
+    })
+
+    new RdsStack(scope, 'RdsStack', vpcStack.vpc, {
+        stackName: `rds-stack-${envType}`
     })
   }
 }
---
+++ project/lib/resources/rdsDatabaseInstance.ts
@@ -0,0 +1,71 @@
+import { Resource } from "./abstract/resource";
+import { Construct } from "constructs";
+import {
+  Vpc, 
+  SecurityGroup,
+  InstanceSize,
+  InstanceType,
+  InstanceClass,
+  Peer,
+  Port 
+} from "aws-cdk-lib/aws-ec2";
+
+import {
+  DatabaseInstance,
+  DatabaseInstanceEngine,
+  MysqlEngineVersion,
+  Credentials,
+  ParameterGroup
+} from "aws-cdk-lib/aws-rds";
+
+export class RdsDatabaseInstance extends Resource {
+ 
+    private readonly vpc: Vpc;
+
+    constructor(vpc: Vpc) {
+      super();
+      this.vpc = vpc;
+    }
+    
+    createResources(scope: Construct) {
+      const envType = scope.node.tryGetContext('envType');
+      const systemName = scope.node.tryGetContext('systemName');
+
+      const dbSg = new SecurityGroup(scope, "rdsSg", { vpc: this.vpc, allowAllOutbound: true });
+
+      this.vpc.selectSubnets( { subnetGroupName: 'app-public' } ).subnets.forEach((x) => {
+        dbSg.addIngressRule(Peer.ipv4(x.ipv4CidrBlock), Port.tcp(3306));
+      })
+
+      const parameterGroup = new ParameterGroup(scope, "RDSParameterGroup", {
+          description: `${systemName}-${envType}-pram-grp`,
+          engine: DatabaseInstanceEngine.mysql({version: MysqlEngineVersion.VER_5_7_34}),
+          parameters: {
+            time_zone: "Asia/Tokyo",
+            character_set_client: "utf8mb4",
+            character_set_connection: "utf8mb4",
+            character_set_database: "utf8mb4",
+            character_set_results: "utf8mb4",
+            character_set_server: "utf8mb4",
+            collation_connection: "utf8mb4_bin",
+          },
+        });
+
+      const rdsCredential = Credentials.fromGeneratedSecret(`${envType}_root`, {
+        secretName: `${envType}/db/credentials`,
+      });
+
+      new DatabaseInstance(scope, 'DbInstance', {
+          databaseName: `${systemName}_${envType}`,
+          instanceIdentifier: `${systemName}-${envType}-db-instance`,
+          engine: DatabaseInstanceEngine.mysql({version: MysqlEngineVersion.VER_5_7_34}),
+          instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.SMALL),
+          parameterGroup,
+          credentials: rdsCredential,
+          vpc: this.vpc,
+          vpcSubnets: this.vpc.selectSubnets( { onePerAz:true, subnetGroupName: 'rds' } ),
+          securityGroups: [dbSg],
+          port: 3306,
+      })   
+    }
+}
